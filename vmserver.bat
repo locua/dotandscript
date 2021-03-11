03ee63ebe87ea863bf448e648902a98fe9ebe604
louis <poes997@gmail.com>
2021-03-11 00:14:11 +0000

    putty and cleaned

--- vmserver.bat
+++ vmserver.bat
@@ -1,10 +1,11 @@
-ECHO hello
+@echo off
+.\VBoxManage.exe list runningvms | find "ubuntu server 1" || ( CALL :Start_server)
+::ssh -p 2222 test@127.0.0.1
+CD "C:\Users\<mmm>"
+putty -ssh -P 2222 -i .ssh\specialkey.ppk user1238313@127.0.0.1
 
-CD  "C:\Program Files\Oracle\VirtualBox>"
-.\VBoxManage.exe list runningvms | find "ubuntu server 1" || ( echo no running servers so... & CALL :Start_server)
-ssh -p 2222 test@127.0.0.1
-
-PAUSE
+::PAUSE
+&& EXIT 0
 
 :Start_server
 .\VBoxManage.exe startvm "ubuntu server 1" --type headless
